167379fb9d312fdb15f134bbff77c384396b9440
neha0305verma <neha0305verma@gmail.com>
2019-10-11 18:19:59 +0530

    code improvement : relationship removed between textbook and concepts, fixed tags and keywords value change

--- platform-modules/common/src/main/java/org/ekstep/common/mgr/ConvertGraphNode.java
+++ platform-modules/common/src/main/java/org/ekstep/common/mgr/ConvertGraphNode.java
@@ -121,8 +121,7 @@ public class ConvertGraphNode {
                 }
                 updateReturnMap(map, outRelMap, outRelDefMap);
             }
-            if (null != node.getTags() && !node.getTags().isEmpty())
-            		map.put("tags", node.getTags());
+
             map.put("identifier", node.getIdentifier());
         }
         return map;
